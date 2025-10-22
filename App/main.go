e5933555a2857e5b1b6aa6e529e5b8171b2e69f3
Singh <gurpreet.singh@invenco.com>
2025-10-22 16:03:45 +0530

    rate-limiter

--- App/main.go
+++ App/main.go
@@ -7,15 +7,22 @@ import (
 	"net/http"
 	"rate-limiter/Middleware"
 	models "rate-limiter/Models"
+	"rate-limiter/Package"
 	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 )
+
+// Collection BluePrint
+type AuthHandler struct {
+	userCollection *mongo.Collection
+}
 
 func main() {
 
@@ -68,46 +75,87 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+	usercollection := mongoClient.Database("rate-limiter").Collection("Users")
+	authHandler := &AuthHandler{
+		userCollection: usercollection,
+	}
+	AggressiveRateLimiter := Package.TokenBuucketAlgorithm(RedisClient, 5, 0.15)
 	//Create Server
 	app := gin.Default()
 	app.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Server Healthy"})
-	})
-	app.POST("/signup", Middleware.CheckUserInput, func(c *gin.Context) {
-		val, exists := c.Get("validatedUser")
-		if !exists {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "error in recieve the input from the middleware"})
-			return
-
-		}
-		newUser := val.(models.User)
-		// if err := c.ShouldBindJSON(&newUser); err != nil {
-		// 	fmt.Println("Error ->", err)
-		// 	c.JSON(http.StatusBadRequest, gin.H{
-
-		// 		"message": "error in reading the body of the request %w",
-		// 	})
-		// 	return
-		// }
-		//Hash the password
-		hashPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"err": "Failed to hash the password"})
-			return
-		}
-		newUser.Password = string(hashPassword)
-		//Create mongo collection
-		collection := mongoClient.Database("rate-limiter").Collection("Users")
-		_, err = collection.InsertOne(context.TODO(), newUser)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "error in putting new user in the database",
-			})
-		}
 		c.JSON(http.StatusOK, gin.H{
-			"messge": "SignUp Successfully",
+			"message": "Server Healthy",
 		})
 	})
 
+	app.POST("/signup", AggressiveRateLimiter, Middleware.CheckUserInput, authHandler.SignUp)
+	app.POST("/login", AggressiveRateLimiter, Middleware.CheckUserInput, authHandler.Login)
 	app.Run()
+}
+func (h *AuthHandler) SignUp(c *gin.Context) {
+	val, exists := c.Get("validatedUser")
+	if !exists {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "error in recieve the input from the middleware",
+		})
+		return
+
+	}
+	newUser := val.(models.User)
+	//Hash the password
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"err": "Failed to hash the password",
+		})
+		return
+	}
+	newUser.Password = string(hashPassword)
+	//Create mongo collection
+	_, err = h.userCollection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "error in putting new user in the database",
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "SignUp Successfully",
+	})
+}
+func (h *AuthHandler) Login(c *gin.Context) {
+	value, exists := c.Get("validatedUser")
+	if !exists {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": "error in getting the value from middleware",
+		})
+		return
+	}
+	loginAttempt := value.(models.User)
+	var userfromDB models.User
+	filter := bson.M{"username": loginAttempt.Username}                         //We create filter because we find to that username
+	err := h.userCollection.FindOne(context.TODO(), filter).Decode(&userfromDB) //I dont know why we use context.todo and why Decode(&userfromDB)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid creditenials",
+			})
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "error in database",
+		})
+		return
+	}
+	//compare the password
+	err = bcrypt.CompareHashAndPassword([]byte(userfromDB.Password), []byte(loginAttempt.Password)) //how userfromDB.password get the password from the database
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"error": "Invalid creditenials",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login Successfully",
+	})
 }
--- Package/TockenBucket.go
+++ Package/TockenBucket.go
@@ -1,89 +1,89 @@
 package Package
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"math"
-// 	"net/http"
-// 	"strconv"
-// 	"time"
+import (
+	"context"
+	"fmt"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
 
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/redis/go-redis/v9"
-// )
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
 
-// var IntailToken int
-// var BucketSize int
+var IntailToken int
+var BucketSize int
 
-// func TokenBuucketAlgorithm(RedisClient *redis.Client, Bucketsize int64, refill float64) gin.HandlerFunc {
-// 	ctx := context.Background()
-// 	return func(c *gin.Context) {
-// 		//Get the Cilent Ip
-// 		ipAddress := c.ClientIP()
-// 		hashkey := fmt.Sprintf("Rate limit %s", ipAddress)
-// 		// fmt.Println("HashKey", hashkey)
+func TokenBuucketAlgorithm(RedisClient *redis.Client, Bucketsize int64, refill float64) gin.HandlerFunc {
+	ctx := context.Background()
+	return func(c *gin.Context) {
+		//Get the Cilent Ip
+		ipAddress := c.ClientIP()
+		hashkey := fmt.Sprintf("Rate limit %s", ipAddress)
+		// fmt.Println("HashKey", hashkey)
 
-// 		var tokens float64
-// 		var lastTimeStap int64
-// 		//Now make the transcationn
-// 		pipe := RedisClient.TxPipeline()
-// 		bucketstate := pipe.HGetAll(ctx, hashkey)
-// 		pipe.Exec(ctx) //for existing the command
-// 		bucketData, err := bucketstate.Result()
-// 		// fmt.Println("Bucket Data ->", bucketData)
-// 		if err != nil || len(bucketData) == 0 {
-// 			//This mean it is new user
-// 			initialToken := Bucketsize - 1
-// 			pipe = RedisClient.TxPipeline()
-// 			pipe.HSet(ctx, hashkey, "token", initialToken)
-// 			pipe.HSet(ctx, hashkey, "timeStap", time.Now().Unix())
-// 			//Persist the token //Clear the token after 1 hour
-// 			pipe.Expire(ctx, hashkey, 1*time.Hour)
-// 			_, err := pipe.Exec(ctx)
-// 			if err != nil {
-// 				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Error in setting the value to the new user"})
-// 				return
+		var tokens float64
+		var lastTimeStap int64
+		//Now make the transcationn
+		pipe := RedisClient.TxPipeline()
+		bucketstate := pipe.HGetAll(ctx, hashkey)
+		pipe.Exec(ctx) //for existing the command
+		bucketData, err := bucketstate.Result()
+		// fmt.Println("Bucket Data ->", bucketData)
+		if err != nil || len(bucketData) == 0 {
+			//This mean it is new user
+			initialToken := Bucketsize - 1
+			pipe = RedisClient.TxPipeline()
+			pipe.HSet(ctx, hashkey, "token", initialToken)
+			pipe.HSet(ctx, hashkey, "timeStap", time.Now().Unix())
+			//Persist the token //Clear the token after 1 hour
+			pipe.Expire(ctx, hashkey, 1*time.Hour)
+			_, err := pipe.Exec(ctx)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Error in setting the value to the new user"})
+				return
 
-// 			}
-// 			c.Next()
-// 			return
-// 		}
-// 		//Check for existing user
-// 		//
-// 		// fmt.Println("Existing user part")
-// 		RemainingTokenStr := bucketData["token"]
-// 		lastTimeStapStr := bucketData["timeStap"]
-// 		RemainingToken, _ := strconv.ParseFloat(RemainingTokenStr, 64)
-// 		lastTimeStap, _ = strconv.ParseInt(lastTimeStapStr, 10, 64)
+			}
+			c.Next()
+			return
+		}
+		//Check for existing user
+		//
+		fmt.Println("Existing user part")
+		RemainingTokenStr := bucketData["token"]
+		lastTimeStapStr := bucketData["timeStap"]
+		RemainingToken, _ := strconv.ParseFloat(RemainingTokenStr, 64)
+		lastTimeStap, _ = strconv.ParseInt(lastTimeStapStr, 10, 64)
 
-// 		//Calcualte the time to add the token
-// 		Now := time.Now().Unix()
-// 		elapsedSecond := Now - lastTimeStap
-// 		//Refill rate
-// 		tokenToAdd := float64(elapsedSecond) * refill
-// 		// fmt.Println("Token to Add", tokenToAdd)
-// 		tokens = math.Min(float64(Bucketsize), RemainingToken+tokenToAdd)
-// 		// fmt.Println(tokens)
+		//Calcualte the time to add the token
+		Now := time.Now().Unix()
+		elapsedSecond := Now - lastTimeStap
+		//Refill rate
+		tokenToAdd := float64(elapsedSecond) * refill
+		// fmt.Println("Token to Add", tokenToAdd)
+		tokens = math.Min(float64(Bucketsize), RemainingToken+tokenToAdd)
+		// fmt.Println(tokens)
 
-// 		if tokens < 1 {
-// 			//Exceed the request abort the chain
-// 			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"message": "Too many requests are made ... Try after some time"})
-// 			return
-// 		}
-// 		RemainingToken = tokens - 1
-// 		pipe = pipe.TxPipeline()
-// 		pipe.HSet(ctx, hashkey, "token", RemainingToken)
-// 		pipe.HSet(ctx, hashkey, "timeStap", Now)
-// 		_, err = pipe.Exec(ctx)
-// 		if err != nil {
-// 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-// 				"message": "Error in setting the value",
-// 			})
-// 			return
+		if tokens < 1 {
+			//Exceed the request abort the chain
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"message": "Too many requests are made ... Try after some time"})
+			return
+		}
+		RemainingToken = tokens - 1
+		pipe = pipe.TxPipeline()
+		pipe.HSet(ctx, hashkey, "token", RemainingToken)
+		pipe.HSet(ctx, hashkey, "timeStap", Now)
+		_, err = pipe.Exec(ctx)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Error in setting the value",
+			})
+			return
 
-// 		}
-// 		fmt.Printf("Request from %s and remaining token %0.2f", ipAddress, RemainingToken)
-// 		c.Next()
+		}
+		fmt.Printf("Request from %s and remaining token %0.2f", ipAddress, RemainingToken)
+		c.Next()
 
-// 	}
-// }
+	}
+}
